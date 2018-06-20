bc5051f40413dbbcfc1fff137a3b93a18491c795
HemersonGH <hemersonel@gmail.com>
2018-06-20 19:28:07 -0300

    Finalizando R3

--- TestUnit/src/br/ufla/dcc/ES/StringCalc.java
+++ TestUnit/src/br/ufla/dcc/ES/StringCalc.java
@@ -5,24 +5,49 @@ public class StringCalc {
 	public static int add(String number) {
 
 		int sum = 0;
-		
+
 		String numb = number.replaceAll(" ","");
-		
+		numb = numb.replaceAll("\n","");
+
 		String a[] = numb.split(",");
+
+		ArrayList numberNeg = new ArrayList<Integer>;
 
 		for (String num : a) {
 			sum += Integer.parseInt(num);
-			
+
+			if (num < 0) {
+				numberNeg.add(num);
+			}
+
 		}
-		
+
+		if (!numberNeg.isEmpty()) {
+			System.out.print("Negatives not allowed: ");
+
+			for (Integer numNeg : numberNeg) {
+				System.out.print(numNeg + ", ");
+			}
+		}
+
 		String total = number.replaceAll(" ","");
 		total = total.replaceAll(",", " + ");
-		
+
 		System.out.print(total + " = ");
-		
+
 		return sum;
 
 	}
+
+	// Test Sum Ok
+
+	// Test Sum Number Negatives
+
+	// Test Sum Delimiter /n
+
+	// Test Sum With Space
+
+	// Test Sum With ,
 
 	public static void main(String[] args) {
 
