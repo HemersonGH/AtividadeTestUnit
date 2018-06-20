1d6310592b2fdca44046a40edb6fb5d0211c5cc6
hemersonGH <hemersonel@gmail.com>
2018-06-20 18:11:10 -0300

    Alterando class Calculadora

--- TestUnit/src/br/ufla/dcc/ES/Calculadora.java
+++ TestUnit/src/br/ufla/dcc/ES/Calculadora.java
@@ -1,9 +1,36 @@
 package br.ufla.dcc.ES;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+
 public class Calculadora {
 	
-	public static void main(String[] args) {
-		
+	private JFrame myView;
+
+	public Calculadora() {
+		viewCalc();
 	}
 
-}
+	public void viewCalc(){
+
+		myView = new JFrame();
+		myView.getContentPane().setBackground(new Color(51, 102, 153));
+		myView.getContentPane().setForeground(new Color(255, 255, 255));
+		myView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myView.setResizable(true);
+		myView.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		myView.setTitle("Calculadora");
+		myView.getContentPane().setFont(new Font("Arial", Font.PLAIN, 12));
+		
+		
+		
+		myView.setSize(485, 240);
+		myView.setMinimumSize(new Dimension(485, 240));
+		myView.setVisible(true);
+		myView.setResizable(true);
+	}
+}	
+
