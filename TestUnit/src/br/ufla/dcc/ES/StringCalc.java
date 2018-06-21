90efc6596d05d256ca69790264c6b004b4421a83
Alexandre Gillo <alexandregrill@gmail.com>
2018-06-20 22:31:25 -0300

    Alterando metodos

--- TestUnit/src/br/ufla/dcc/ES/StringCalc.java
+++ TestUnit/src/br/ufla/dcc/ES/StringCalc.java
@@ -1,58 +1,90 @@
 package br.ufla.dcc.ES;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StringCalc {
-
-	public static int add(String number) {
-
-		int sum = 0;
-
-		String numb = number.replaceAll(" ","");
-		numb = numb.replaceAll("\n","");
+	
+	public ArrayList<Integer> convertNumbers(String number){
+		ArrayList<Integer> numbers = new ArrayList<>();
+		
+		String numb = number.replaceAll(" ", "");
+		numb = numb.replaceAll("\n", "");
 
 		String a[] = numb.split(",");
 
-		ArrayList numberNeg = new ArrayList<Integer>;
-
 		for (String num : a) {
-			sum += Integer.parseInt(num);
-
-			if (num < 0) {
-				numberNeg.add(num);
-			}
-
+			numbers.add(Integer.parseInt(num));
 		}
 
-		if (!numberNeg.isEmpty()) {
-			System.out.print("Negatives not allowed: ");
-
-			for (Integer numNeg : numberNeg) {
-				System.out.print(numNeg + ", ");
+		return numbers;
+	}
+	
+	public void checkNegatives(ArrayList<Integer> number) {
+		ArrayList<Integer> numbersNeg = new ArrayList<>();
+		
+		for (Integer integer : number) {
+			if (integer < 0) {
+				numbersNeg.add(integer);
 			}
 		}
+		
+		if (!numbersNeg.isEmpty()) {
+			System.out.print("Negatives Not Allowed: ");
+			
+			for (Integer integer : numbersNeg) {
+				System.out.print(integer + " ");
+			}
+		}
+	}
 
-		String total = number.replaceAll(" ","");
-		total = total.replaceAll(",", " + ");
+	public static int add(ArrayList<Integer> number) {
 
-		System.out.print(total + " = ");
-
+		int sum = 0;
+		
+		for (Integer integer : number) {
+			sum =+ integer;
+		}		
+		
 		return sum;
-
 	}
 
 	// Test Sum Ok
+	public void testSumNumber() {
+		
+	}
 
 	// Test Sum Number Negatives
-
+	public void testSumNegatives() {
+		
+	}
+	
 	// Test Sum Delimiter /n
-
+	public void testSumWithDelimiter() {
+		
+	}
+	
 	// Test Sum With Space
-
+	public void testSumWithSpace() {
+		
+	}
+	
 	// Test Sum With ,
-
+	public void testSumWithVirg() {
+		
+	}
+	
 	public static void main(String[] args) {
+		
+		System.out.println("Digite os números:");
 
-		System.out.println(add("1,   2, 3"));
-
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		
+		String numbers = in.nextLine();
+		
+		
+		
 	}
 
 }
